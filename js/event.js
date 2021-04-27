7229993b045fa5931d5bd59d29c7f3e2e946562d
Mahmood <48608374+mahmood93m@users.noreply.github.com>
2021-04-27 12:37:40 -0500

    add java sciprt file

---
+++ js/event.js
@@ -0,0 +1,15 @@
+/* Slider JS */
+var myIndex = 0;
+chicago();
+
+function chicago() {
+  var i;
+  var x = document.getElementsByClassName("slides");
+  for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}
+  x[myIndex-1].style.display = "block";
+  setTimeout(chicago, 5000);
+}
